2495a5806dbad47090099b3577e9ef3f7f7f9054
Artheg <artheg@pm.me>
2022-08-17 16:37:31 +0300

    ALE Fix on save;

--- init.lua
+++ init.lua
@@ -192,6 +192,7 @@ dap.configurations.typescript = { -- change to typescript if needed
   vim.g['ale_javascript_eslint_use_global'] = 1
   vim.g['ale_typescript_eslint_executable'] = 'eslint_d'
   vim.g['ale_typescript_eslint_use_global'] = 1
+  vim.g['ale_fix_on_save'] = 1
   ----
 
   ---- better word motion (e.g. CamelCase)
